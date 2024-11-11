d92ee765f6f4f59d98b933704e1d9bf42678b701
Roman Subbotin <roman.subbotin96@yandex.ru>
2024-11-11 22:09:55 +0300

    Работа с адаптивом

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 tailwind.config = {
     theme: {
-      extend: {
-        colors: {
-          primary: '#3865A7',
-          secondary: '#FE5F5F',
-          dark: '#2F2F2F',
-          'primary-hover': '#2d5490',
-          'secondary-hover': '#e54545',
-        }
-      }
-    }
-  }
+        extend: {
+            colors: {
+                primary: '#3865A7',
+                secondary: '#FE5F5F',
+                dark: '#2F2F2F',
+                'primary-hover': '#2d5490',
+                'secondary-hover': '#e54545',
+            },
+        },
+    },
+};
